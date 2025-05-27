39f4153b90b741691d5d991e8af35a36a333a87b
sajin5263 <sajin17102001@gmail.com>
2025-05-27 11:33:21 +0530

    added

--- demoproject/src/main/java/com/corfield/demoproject/Controller/UserControllers.java
+++ demoproject/src/main/java/com/corfield/demoproject/Controller/UserControllers.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@RequestMapping("/viewall")
+@RequestMapping("/viewa")
 public class UserControllers {
 
     @Autowired
